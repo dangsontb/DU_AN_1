9a4629a7b76fd306b5c549d51d08a8ab15ad970c
dangsontb <dangvanson210297@gmail.com>
2024-03-22 13:28:09 +0700

    index.php

--- admin/index.php
+++ admin/index.php
@@ -17,49 +17,11 @@
                 include "category/list_cate.php";
                 break;
 
-                case 'delete':
-                    if(isset($_GET['id']) &&  ($_GET['id']) > 0){
-                       delete_category($_GET['id']);
-                    } 
-                    $list_category = category_select_all();
-
-                    include "category/list_cate.php";
-               break;
-
-               case 'add_cate':
-                if(isset($_POST['themmoi']) && ($_POST['themmoi'])){
-                   
-                       $cate_name=$_POST['cate_name'];
-                       category_insert($cate_name);
-                   }
-                include 'category/add_cate.php';
-                break;
-
-            case 'edit':
-               if(isset($_GET['id']) &&  ($_GET['id']) > 0){
-                $show_one_cate =  category_select_by_id($_GET['id']);
-                } 
-             
-               include 'category/update_cate.php';  
-               break;  
-               
-             case 'update_cate':
-               if(isset($_POST['update']) && ($_POST['update'])){
-                $cate_id=$_POST['cate_id'];
-                   $cate_name=$_POST['cate_name'];
-                   category_update($cate_id,$cate_name);
-               }
-               $list_category = category_select_all();
-               include 'category/list_cate.php';
-               break;  
-
             // --------------------------BRAND----------------------------------
             case 'list_brand':
                 $list_brand = brand_select_all();
                 include "brand/list_brand.php";
                 break;
-
-
             case 'add_brand':
                 if(isset($_POST['themmoi']) && $_POST['themmoi']){
                     $brand_name = $_POST['brand_name'];
@@ -76,32 +38,41 @@
                 }
                 include "brand/add_brand.php";
                 break;
-             case 'delete':
-                     if(isset($_GET['id']) &&  ($_GET['id']) > 0){
-                        delete_brand($_GET['id']);
-                     } 
-                     $list_brand = brand_select_all();
 
-                include 'brand/list_brand.php';
+            case 'edit':
+                if(isset($_GET['id']) && ($_GET['id']) >0 ){        
+                    $show_one_brand = brand_select_by_id($_GET['id']);
+                }               
+                include 'brand/update_brand.php';
                 break;
 
-             case 'edit':
-                if(isset($_GET['id']) &&  ($_GET['id']) > 0){
-                  $show_one_brand =  brand_select_by_id($_GET['id']);
-                 } 
-              
-                include 'brand/update_brand.php';
-                break;  
-                
-              case 'update_brand':
-                if(isset($_POST['update']) && ($_POST['update'])){
-                    $brand_id=$_POST['brand_id'];
-                    $brand_name=$_POST['brand_name'];
-                    brand_update($brand_id,$brand_name);
+            case 'update_brand':   
+                if(isset($_POST['update']) && $_POST['update']){
+                    $brand_id = $_POST['brand_id'];
+                    $brand_name = $_POST['brand_name'];
+                    
+                    if(!empty($brand_name)){
+                        $brand_name_unique = brand_select_name($brand_name);
+                        if(is_array($brand_name_unique)){
+                            $brand_name_error = "Tên này đã tồn tại";
+                        }else{
+                            brand_update($brand_id,$brand_name);         
+                        }
+                    }else $brand_name_error = "Không được để trống !";
+                    
                 }
                 $list_brand = brand_select_all();
                 include 'brand/list_brand.php';
-                break;  
+                break;    
+
+            case 'delete':
+                if(isset($_GET['id']) && ($_GET['id']) >0 ){
+                    delete_brand($_GET['id']);
+                }
+                $list_brand = brand_select_all();
+                include "brand/list_brand.php";
+                break;
+                    
             // --------------------------PRODUCT----------------------------------
             case 'list_product':
                 if(isset($_POST['themmoi']) && $_POST['themmoi']){
@@ -112,15 +83,57 @@
                 break;
             case 'add_product':
                 if(isset($_POST['themmoi']) && $_POST['themmoi']){
-                   $name = $_POST['name'] ;
-                   $price = $_POST['price'] ;
-                   $image = $_FILES['image'] ;
-                   $description = $_POST['description'] ;
-                   $sale = $_POST['sale'] ;
-                   $create_at = $_POST['create_at'] ;
-                   $view = $_POST['view'] ;
-                   $cate_id  = $_POST['cate_id '] ;
-                   $brand_id  = $_POST['brand_id '] ;
+                    $name = $_POST['name'] ;
+                    $price = $_POST['price'] ;
+                    $image = $_FILES['image']['name'] ;
+                    
+                    $description = $_POST['description'] ;
+                    $sale = $_POST['sale'] ;
+                    $create_at = $_POST['create_at'] ;
+                    $view = $_POST['view'] ;
+                    $cate_id  = $_POST['cate_id '] ;
+                    $brand_id  = $_POST['brand_id '] ;
+                    // $image_description = $_FILES['img_product']['name'] ;
+                    $image_tail = strtolower(pathinfo($image, PATHINFO_EXTENSION));
+                    // $image_description_tail = strtolower(pathinfo($image, PATHINFO_EXTENSION));
+                    $tails = ['png','jpg','jpeg','pdf'];
+
+                    $error =[];
+
+                    if(!empty($name)){
+                        if(strlen($name) > 255 ) $error['name'] = "Tên phải ngắn hơn 255 ký tự";
+                    }else{
+                        $error['name'] ="Nhập tên sản phẩm !" ;
+                    }
+
+                    if(!empty($price)){
+                        if($price < 0 ) $error['price'] = "Giá phải lớn hơn 0";
+                    }else{
+                        $error['price'] = "Nhập giá sản phẩm" ;
+                    }
+
+                    if(!empty($image)){
+                        if(!in_array($image_tail, $tails)  && filesize($image) > 8192){
+                            $error['image'] = "Phải là ảnh  và kích thước nhỏ hơn 1mb";
+                        }
+                    }else{
+                        $error['image'] = "Vui lòng chọn ảnh";
+                    }
+
+                    if(empty($description)){
+                        $error['description'] = "Nhập mô tả" ;
+                    }
+                    
+
+
+                    // if(!empty($image_description)){
+                    //     if(!in_array($image_description_tail, $tails)  && filesize($image_description) > 8192){
+                    //         $error['image_description'] = "Phải là ảnh  và kích thước nhỏ hơn 1mb";
+                    //     }
+                    // }else{
+                    //     $error['image_description'] = "Vui lòng chọn ảnh";
+                    // }
+
                 }
                 include 'product/add_product.php';
                 break;
