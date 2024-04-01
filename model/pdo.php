998dcf7c8e14a3e3bc35948d27b57ae5eaba9ac0
phamthihongngat <hongngat161102@hmail.com>
2024-04-01 20:21:54 +0700

    update giỏ hàng, thanh toán

--- model/pdo.php
+++ model/pdo.php
@@ -88,24 +88,24 @@
 
 
     
-    function insert_id(){
+    // function insert_id(){
         
-        return pdo_get_connection() -> lastInsertId();
-    }
-
-    // function pdo_execute_return_lastInsertId($sql)
-    // {
-    //     $sql_args = array_slice(func_get_args(), 1);
-    //     try {
-    //         $conn = pdo_get_connection();
-    //         $stmt = $conn->prepare($sql);
-    //         $stmt->execute($sql_args);
-    //         return $conn->lastInsertId();
-    //     } catch (PDOException $e) {
-    //         throw $e;
-    //     } finally {
-    //         unset($conn);
-    //     }
+    //     return pdo_get_connection() -> lastInsertId();
     // }
+
+    function insert_id($sql)
+    {
+        $sql_args = array_slice(func_get_args(), 1);
+        try {
+            $conn = pdo_get_connection();
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($sql_args);
+            return $conn->lastInsertId();
+        } catch (PDOException $e) {
+            throw $e;
+        } finally {
+            unset($conn);
+        }
+    }
    
 ?>
