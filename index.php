703957e3968bde98bfe2ede3da83e3c1be4e2ddf
NghiemCongHieu <nghiemconghieu2004@gmail.com>
2024-04-04 13:04:25 +0700

    a

--- index.php
+++ index.php
@@ -98,12 +98,9 @@
             case 'signup':
                 signup();
                 break;
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> 6bf12487ee2394c4dabecac8a6ebebeba8ea56ef
             // --------------------------------------------------------------- Giỏ hàng -----------------------------------------------------------
             case 'viewcart':
                 include "views/cart/viewcart.php";
@@ -172,10 +169,7 @@
                 // }else{
                 //     header("location: index.php");
                 break;
-<<<<<<< HEAD
     
-=======
->>>>>>> 6bf12487ee2394c4dabecac8a6ebebeba8ea56ef
             default:
                 include "views/home.php";
                 break;
